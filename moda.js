082c241657bce6bf38a53db5501535dd67751859
Ibrahim Gragirena <ibrach98@gmail.com>
2022-07-14 02:16:05 -0300

    Debug

--- moda.js
+++ moda.js
@@ -11,25 +11,33 @@ const lista = [
     10,
 ]
 
-const listaCount = {};
-lista.map(
-    function (elemento) {
-        if (listaCount[elemento]){
-            listaCount[elemento] += 1;
-        }
-        else {
-            listaCount[elemento] = 1
-        }
-    }
-);
+function displayModa() 
+  {
 
-const arrList = Object.entries(listaCount).sort(
-    function (valorAcumulado, nuevoValor) {
-        valorAcumulado[1] - nuevoValor[1];
-    }
-);
+    const listaCount = {};
+    lista.map(
+        function (elemento) {
+            if (listaCount[elemento]){
+                listaCount[elemento] += 1;
+            }
+            else {
+                listaCount[elemento] = 1
+            }
+        }
+    );
+    
+    const arrList = Object.entries(listaCount).sort(
+        function (a, b) {
+           return a[1] - b[1];
+        }
+    );
 
-const moda = arrList[arrList.length - 1];
+    const moda = arrList[arrList.length - 1];
+
+    const content = moda ;
+    document.getElementById('display').innerHTML = "La moda es : " + content[0] + " Se repite " + moda[1] + " veces";
+  }
+
 
 function savedata(){
     const inputAge = document.getElementById('Age').value;  
@@ -41,9 +49,5 @@ function displayList()
     document.getElementById('display').innerHTML = "Edades : " + content;
   }
 
-function displayModa() 
-  {
-    const content = moda ;
-    document.getElementById('display').innerHTML = "La moda es : " + content[0] + " Se repite " + moda[1] + " veces";
-  }
+
 
